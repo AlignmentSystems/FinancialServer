3a0ee5e9bae87083f046d0789e99248b00eb0d4a
JohnGreenan <enquire@alignment-systems.com>
2014-09-30 13:45:46 +0100

    Added client code to parse and display json
    First draft of client-side javascript to receive and parse

--- index.js
+++ index.js
@@ -3,26 +3,32 @@
  * Financial Server - a node.js example of a simplified financial markets real-time data server
  * Typical use case: Allow clients of an financial services firm to see the current bid and ask prices of
  * stocks in their portfolio in real time.
- *  
+ * 
+ * Author   :   John Greenan
+ * Date     :   30th September 2014 
+ * 
+ * 
  */
 var app = require('express')();
-var path = require('path');
-var events = require('events');
 var math = require('mathjs');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var TargetPort = 3000;
+var path = require('path');
 
 //JSON to hold a structure for a real-time price update...
 var RealTimePriceTick = {
-    "stock"     : "ABC",
+    "Stock"     : "ABC",
     "LastTrade" : "101",
     "Bid"       : "101",
     "Ask"       : "101",
-    "Volume"    : "101"
+    "Volume"    : "101",
+    "Timestamp" : "101",
+    "Side"      : "B",
 }; 
   
 app.get('/', function (req, res) {
+    console.log('Received request for ' + req.url);
     res.sendFile(path.join(__dirname,'/Index.html'));
 });
 
@@ -32,21 +38,11 @@ http.listen(TargetPort, function () {
 
 io.on('connection', function (socket) {
     console.log('a user connected')
-    
-    //How do we check to see if a socket has actually been created?
-    //So at a simple level - like in VBA "if not xxx is nothing then... else...end if?
-    //Possibly just me bring my legacy coding baggage?
-    //If we stick this disconnect event within the connection event code then we MUST have connected, so a socket must be here
-    //I would prefer to have this piece of logic somewhere else, so we don't have much nesting of functions, again, this is probably
-    //just a hang up from VBA days....
-    
+        
     socket.on('disconnect', function () {
         console.log('a user disconnected');
     });
-
-
 });
-
 
 var Inc = function (startnumber) {
     this.count = startnumber;
@@ -59,6 +55,14 @@ Inc.prototype.increment = function () {
     
     setInterval(function () {
         var StockSwitcher = math.random();
+        
+        if (StockSwitcher > 0.0) {
+            RealTimePriceTick.Side = 'B';
+        }
+        
+        if (StockSwitcher > 0.5) {
+            RealTimePriceTick.Side = 'S';
+        }
         
         if (StockSwitcher > 0.0) {
             RealTimePriceTick.stock = 'JKL';
@@ -75,12 +79,13 @@ Inc.prototype.increment = function () {
         if (StockSwitcher > 0.75) {
             RealTimePriceTick.stock = 'ABC';
         }
-        var Ask = math.floor((100 * math.random()) + 1)
-        var Bid = Ask * 1.023;
+        var Bid = math.floor((100 * math.random()) + 1)
+        var Ask = Bid * 1.023;
         var LastTrade = (Ask + Bid) / 2;
         RealTimePriceTick.Ask = Ask.toFixed(2);
         RealTimePriceTick.Bid = Bid.toFixed(2);
-        RealTimePriceTick.LastTrade=LastTrade.toFixed(2) ;
+        RealTimePriceTick.LastTrade = LastTrade.toFixed(2);
+        RealTimePriceTick.Timestamp = new Date().toJSON();
         RealTimePriceTick.Volume = self.count;
         self.emit('Tick',RealTimePriceTick);
         self.count++;
